e9382f63a5549302440a6b0f2b4acb27d6b983e6
André <AndreR@ppedv.de>
2022-08-11 11:36:23 +0200

    Cars add and read with AutoFixture

--- ppedv.CarManager/ppedv.CarManager.Data.EfCore.Tests/EfContextTests.cs
+++ ppedv.CarManager/ppedv.CarManager.Data.EfCore.Tests/EfContextTests.cs
@@ -1,4 +1,7 @@
+using AutoFixture;
+using AutoFixture.Kernel;
 using ppedv.CarManager.Model;
+using System.Reflection;
 
 namespace ppedv.CarManager.Data.EfCore.Tests
 {
@@ -16,7 +19,7 @@ namespace ppedv.CarManager.Data.EfCore.Tests
         [Fact]
         public void Can_add_and_read_Car()
         {
-            var car = new Car() { Manufacturer = "Baudi" };
+            var car = new Car() { Manufacturer = $"Baudi_{Guid.NewGuid()}" };
 
             using (var con = new EfContext())
             {
@@ -35,8 +38,8 @@ namespace ppedv.CarManager.Data.EfCore.Tests
         [Fact]
         public void Can_update_Car()
         {
-            var car = new Car() { Manufacturer = "Baudi" };
-            var newManu = "Bolvo";
+            var car = new Car() { Manufacturer = $"Baudi_{Guid.NewGuid()}" };
+            var newManu = $"Bolvo_{Guid.NewGuid()}";
 
             using (var con = new EfContext())
             {
@@ -61,7 +64,7 @@ namespace ppedv.CarManager.Data.EfCore.Tests
         [Fact]
         public void Can_delete_Car()
         {
-            var car = new Car() { Manufacturer = "Baudi" };
+            var car = new Car() { Manufacturer = $"Baudi_{Guid.NewGuid()}" };
 
             using (var con = new EfContext())
             {
@@ -81,6 +84,47 @@ namespace ppedv.CarManager.Data.EfCore.Tests
                 var loaded = con.Find<Car>(car.Id);
                 Assert.Null(loaded);
             }
+        }
+
+        [Fact]
+        public void Can_add_and_read_Car_with_AutoFixture()
+        {
+            var fix = new Fixture();
+            fix.Behaviors.Add(new OmitOnRecursionBehavior());
+            fix.Customizations.Add(new PropertyNameOmitter(nameof(Entity.Id)));
+            var car = fix.Create<Car>();
+
+            using (var con = new EfContext())
+            {
+                con.Cars.Add(car);
+                Assert.Equal(2, con.SaveChanges());
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Find<Car>(car.Id);
+                Assert.NotNull(loaded);
+                Assert.Equal(car.Manufacturer, loaded.Manufacturer);
+            }
+        }
+    }
+
+    internal class PropertyNameOmitter : ISpecimenBuilder
+    {
+        private readonly IEnumerable<string> names;
+
+        internal PropertyNameOmitter(params string[] names)
+        {
+            this.names = names;
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var propInfo = request as PropertyInfo;
+            if (propInfo != null && names.Contains(propInfo.Name))
+                return new OmitSpecimen();
+
+            return new NoSpecimen();
         }
     }
 }
