8791bca0376f867102cf8f267e0396fdda5051e2
André <AndreR@ppedv.de>
2022-08-11 10:10:58 +0200

    EfCore Config

---
+++ ppedv.CarManager/ppedv.CarManager.Data.EfCore/EfContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.CarManager.Model;
+
+namespace ppedv.CarManager.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Garage> Garages { get; set; }
+
+        string conString;
+
+        public EfContext(string conString = "Server=(localdb)\\mssqllocaldb;Database=Cars_DEV;Trusted_Connection=true")
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(conString);
+        }
+    }
+}
